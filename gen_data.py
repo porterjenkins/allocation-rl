8e7c388a5f48ce94d7d4679b3e61c1a521c8d8e0
Porter Jenkins <porterjenkins@gmail.com>
2019-07-17 18:17:12 -0700

    Refactor gen_data.py into a class

--- gen_data.py
+++ gen_data.py
@@ -3,56 +3,74 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-N_REGIONS = 4
-N_PRODUCTS = 4
-T = 20
-PRICES = [2.50, 4.99, 3.00, 1.20]
-PLOT = True
-
-def gen_q(prod, t, r):
-    product_w = np.array([8.0, 4.0, 75.0, 1.5])
-    time_w = 2.5
-
-    lmbda = np.dot(product_w, prod) + t*time_w
-    q = np.random.poisson(lam=lmbda, size=r)
-    return q
-
-quantity_data = np.zeros(shape=(T*N_PRODUCTS*N_REGIONS, 4))
-sales_data = np.zeros(shape=(T*N_PRODUCTS*N_REGIONS))
-
-cntr = 0
-for t in range(T):
-    for p in range(N_PRODUCTS):
-        for r in range(N_REGIONS):
-            p_vec = np.zeros(N_PRODUCTS)
-            p_vec[p] = 1.0
-            q_tpr = gen_q(p_vec, t, 1)
-            s_tpr = q_tpr*PRICES[int(p)]
-
-            quantity_data[cntr, 0] = t
-            quantity_data[cntr, 1] = p
-            quantity_data[cntr, 2] = r
-            quantity_data[cntr, 3] = q_tpr
 
 
-            sales_data[cntr] = s_tpr
+class DataGenerator(object):
 
-            cntr+=1
+    def __init__(self,  n_regions, n_products, max_t, prices, plot):
+        self.n_regions = n_regions
+        self.n_products = n_products
+        self.max_t = max_t
+        self.prices = prices
+        self.plot = plot
 
 
-cols = ['time', 'product', 'region', 'quantity']
-data = pd.DataFrame(quantity_data, columns=cols)
-data['sales'] = sales_data
-print(data.head())
 
-data.to_csv("test-data.csv")
+    def gen_q(self, prod, t, r):
+        product_w = np.array([8.0, 4.0, 75.0, 1.5])
+        time_w = 2.5
 
-if PLOT:
+        lmbda = np.dot(product_w, prod) + t*time_w
+        q = np.random.poisson(lam=lmbda, size=r)
+        return q
 
-    sns.set(style="darkgrid")
-    sns.relplot(x="time", y="quantity",
-                 style="region",  kind="line",
-                 data=data)
+    def run(self):
+        quantity_data = np.zeros(shape=(self.max_t*self.n_products*self.n_regions, 4))
+        sales_data = np.zeros(shape=(self.max_t*self.n_products*self.n_regions))
 
-    plt.show()
+        cntr = 0
+        for t in range(self.max_t):
+            for p in range(self.n_products):
+                for r in range(self.n_regions):
+                    p_vec = np.zeros(self.n_products)
+                    p_vec[p] = 1.0
+                    q_tpr = self.gen_q(p_vec, t, 1)
+                    s_tpr = q_tpr*self.prices[int(p)]
 
+                    quantity_data[cntr, 0] = t
+                    quantity_data[cntr, 1] = p
+                    quantity_data[cntr, 2] = r
+                    quantity_data[cntr, 3] = q_tpr
+
+
+                    sales_data[cntr] = s_tpr
+
+                    cntr+=1
+
+
+        cols = ['time', 'product', 'region', 'quantity']
+        data = pd.DataFrame(quantity_data, columns=cols)
+        data['sales'] = sales_data
+        print(data.head())
+
+        data.to_csv("test-data.csv")
+
+        if self.plot:
+
+            sns.set(style="darkgrid")
+            sns.relplot(x="time", y="quantity",
+                         style="region",  kind="line",
+                         data=data)
+
+            plt.show()
+
+
+
+if __name__ == "__main__":
+    n_regions = 4
+    n_products = 4
+    T = 20
+    prices = [2.50, 4.99, 3.00, 1.20]
+    plot = True
+
+    generator = DataGenerator(n_regions, n_products, T, prices, plot)
