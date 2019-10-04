12a788a63b56d7250e809d7f6596ea6d92caa841
Porter Jenkins <porterjenkins@gmail.com>
2019-10-04 14:17:17 -0400

    Add file to create example figure in paper

---
+++ experiments/example_fig.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+
+
+board_config = np.zeros((4, 5))
+board_config[(0,3)] = 1.0
+board_config[(3,1)] = 1.0
+board_config[(1,2)] = 1.0
+board_config[(2,0)] = 1.0
+board_config[(3,4)] = 1.0
+
+
+fig, ax = plt.subplots()
+# define the colors
+cmap = mpl.colors.ListedColormap(['lightgray', 'blue'])
+# create a normalize object the describes the limits of
+# each color
+bounds = [0., 0.5, 1.]
+norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+#plt.imshow(board_config.astype(int), interpolation='none', cmap=cmap, norm=norm)
+sns.heatmap(board_config.astype(int), cmap=cmap,cbar=False)
+
+plt.xlabel('Products')
+plt.ylabel("Regions")
+plt.savefig("figs/example-fig-heatmap.pdf")
