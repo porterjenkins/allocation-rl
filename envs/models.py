4de63170fff0a57553e93d00965cd0d3b8c90418
Porter Jenkins <porterjenkins@gmail.com>
2019-10-04 09:51:59 -0400

    Fix HUGE bug in hier. model. Add true priors over all params. Revert prior values

--- envs/models.py
+++ envs/models.py
@@ -34,25 +34,41 @@ class LinearModel(Model):
                               shape=self.n_regions)
 
             # Generate Product weights
-            w_p = pm.MvNormal('w_p', mu=self.prior.loc_w_p, cov=self.prior.scale_w_p,
+            packed_L_p = pm.LKJCholeskyCov('packed_L_p', n=self.n_products,
+                                           eta=2., sd_dist=pm.HalfCauchy.dist(2.5))
+
+            L_p = pm.expand_packed_triangular(self.n_products, packed_L_p)
+
+            mu_p = pm.MvNormal("mu_p", mu=self.prior.loc_w_p, cov=self.prior.scale_w_p,
+                               shape=self.n_products)
+
+            w_p = pm.MvNormal('w_p', mu=mu_p, chol=L_p,
                               shape=self.n_products)
-            # Prior for customer weight
-            #w_c = pm.Normal('w_c', mu=self.prior.loc_w_c, sigma=self.prior.scale_w_c)
-            # Generate sales weight
-            w_s = pm.TruncatedNormal('w_s', mu=self.prior.loc_w_s, sigma=self.prior.scale_w_s,lower=0.0)
+
+
+            # Generate previous sales weight
+            loc_w_s = pm.HalfCauchy('loc_w_s',1.0)
+            scale_w_s = pm.HalfCauchy('scale_w_s', 2.5)
+
+            w_s = pm.TruncatedNormal('w_s', mu=loc_w_s, sigma=scale_w_s,lower=0.0)
+
             # Generate temporal weights
-            w_t = pm.MvNormal('w_t', mu=self.prior.loc_w_t, cov=self.prior.scale_w_t,
+            packed_L_t = pm.LKJCholeskyCov('packed_L_t', n=self.n_temporal_features,
+                                         eta=2., sd_dist=pm.HalfCauchy.dist(2.5))
+            L_t = pm.expand_packed_triangular(self.n_temporal_features, packed_L_t)
+            mu_t = pm.MvNormal("mu_t", mu=self.prior.loc_w_t, cov=self.prior.scale_w_t,
+                               shape = self.n_temporal_features)
+
+            w_t = pm.MvNormal('w_t', mu=mu_t, chol=L_t,
                               shape=self.n_temporal_features)
 
-            #bias_c_t = pm.Normal("bias_c_t", mu=0.0, sigma=25.0)
             lambda_c_t = pm.math.dot(self.X_temporal, w_t.T)
-            print("lambda_c_t", lambda_c_t.tag.test_value.shape)
 
-            #sigma_c_t = pm.InverseGamma("sigma_c_t",alpha=self.prior.loc_sigma_c_t, beta=self.prior.scale_sigma_c_t)
-            #c_t = pm.Normal("customer_t", mu=lambda_c_t, sigma=sigma_c_t, shape=self.X_temporal.shape.eval()[0])
-            #c_all = c_t[self.time_stamps] * w_c
 
-            bias_q = pm.Normal("bias_q", mu=0.0, sigma=25.0)
+            bias_q_loc = pm.Normal('bias_q_loc', mu=0.0, sigma=1.0)
+            bias_q_scale = pm.HalfCauchy('bias_q_scale', 5.0)
+
+            bias_q = pm.Normal("bias_q", mu=bias_q_loc, sigma=bias_q_scale)
             # TODO: should force mean to be positive ? exp(mu)
             lambda_q = bias_q + lambda_c_t[self.time_stamps] + pm.math.dot(self.X_region, w_r.T) + pm.math.dot(self.X_product, w_p.T)  + w_s * self.X_lagged
 
@@ -75,32 +91,47 @@ class HierarchicalModel(Model):
         with pm.Model() as env_model:
 
             # Generate upper-level region weights
-            w_r = pm.MvNormal('w_r', mu=self.prior.loc_w_r, cov=self.prior.scale_w_r,
+            w_r = pm.MvNormal('w_r', mu=self.prior.loc_w_r, tau=self.prior.scale_w_r,
                               shape=self.n_regions)
 
             # Generate lower-level region weights
-            w_r_ij = pm.MvNormal('w_r_ij', mu=w_r, cov=self.prior.scale_w_r, shape=(self.n_products, self.n_regions))
+            w_r_ij = pm.MvNormal('w_r_ij', mu=w_r, tau=self.prior.scale_w_r, shape=(self.n_products, self.n_regions))
             #w_r_ij = w_r_ij_gen[self.product_idx, :]
 
             # Generate Product weights
-            w_p = pm.MvNormal('w_p', mu=self.prior.loc_w_p, cov=self.prior.scale_w_p,
+            packed_L_p = pm.LKJCholeskyCov('packed_L_p', n=self.n_products,
+                                           eta=2., sd_dist=pm.HalfCauchy.dist(2.5))
+
+            L_p = pm.expand_packed_triangular(self.n_products, packed_L_p)
+
+            mu_p = pm.MvNormal("mu_p", mu=self.prior.loc_w_p, cov=self.prior.scale_w_p,
+                               shape=self.n_products)
+
+            w_p = pm.MvNormal('w_p', mu=mu_p, chol=L_p,
                               shape=self.n_products)
 
-            # Prior for customer weight
-            #w_c = pm.Normal('w_c', mu=self.prior.loc_w_c, sigma=self.prior.scale_w_c)
+            # Generate previous sales weight
+            loc_w_s = pm.HalfCauchy('loc_w_s', 1.0)
+            scale_w_s = pm.HalfCauchy('scale_w_s', 2.5)
 
-            # Generate customer weight
-            w_s = pm.TruncatedNormal('w_s', mu=self.prior.loc_w_s, sigma=self.prior.scale_w_s, lower=0.0)
+            w_s = pm.TruncatedNormal('w_s', mu=loc_w_s, sigma=scale_w_s, lower=0.0)
 
             # Generate temporal weights
-            w_t = pm.MvNormal('w_t', mu=self.prior.loc_w_t, cov=self.prior.scale_w_t,
+            packed_L_t = pm.LKJCholeskyCov('packed_L_t', n=self.n_temporal_features,
+                                           eta=2., sd_dist=pm.HalfCauchy.dist(2.5))
+            L_t = pm.expand_packed_triangular(self.n_temporal_features, packed_L_t)
+            mu_t = pm.MvNormal("mu_t", mu=self.prior.loc_w_t, cov=self.prior.scale_w_t,
+                               shape=self.n_temporal_features)
+
+            w_t = pm.MvNormal('w_t', mu=mu_t, chol=L_t,
                               shape=self.n_temporal_features)
+
             lambda_c_t = pm.math.dot(self.X_temporal, w_t.T)
 
-            #c_t = pm.Normal("customer_t", mu=lambda_c_t, sigma=25.0,shape=self.X_temporal.shape.eval()[0])
-            #c_all = c_t[self.time_stamps] * w_c
+            bias_q_loc = pm.Normal('bias_q_loc', mu=0.0, sigma=1.0)
+            bias_q_scale = pm.HalfCauchy('bias_q_scale', 5.0)
 
-            bias_q = pm.Normal("bias_q", mu=0.0, sigma=25.0)
+            bias_q = pm.Normal("bias_q", mu=bias_q_loc, sigma=bias_q_scale)
             lambda_q = bias_q + pm.math.sum(self.X_region * w_r_ij[self.product_idx], axis=1) + pm.math.dot(self.X_product, w_p.T) + \
                        lambda_c_t[self.time_stamps] + w_s * self.X_lagged
 
