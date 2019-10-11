515731ab4ed9eb7563d7c777d00e3b4f90962692
Porter Jenkins <porterjenkins@gmail.com>
2019-10-11 15:22:57 -0400

    update example figure

--- experiments/example_fig.py
+++ experiments/example_fig.py
@@ -20,10 +20,40 @@ cmap = mpl.colors.ListedColormap(['lightgray', 'blue'])
 bounds = [0., 0.5, 1.]
 norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
 #plt.imshow(board_config.astype(int), interpolation='none', cmap=cmap, norm=norm)
-sns.heatmap(board_config.astype(int), cmap=cmap,cbar=False)
+sns.heatmap(board_config.astype(int), cmap=cmap,cbar=False, linewidths=.5)
 
 plt.xlabel('Products', fontsize=16)
 plt.ylabel("Regions", fontsize=16)
 plt.xticks(fontsize=14)
 plt.yticks(fontsize=14)
 plt.savefig("figs/example-fig-heatmap.pdf")
+
+
+A = np.zeros((4, 4))
+A[0, 1] = 1.0
+A[0, 3] = 1.0
+A[1, 0] = 1.0
+A[1, 3] = 1.0
+A[1, 2] = 1.0
+A[2, 1] = 1.0
+A[2, 3] = 1.0
+A[3, 0] = 1.0
+A[3, 1] = 1.0
+A[3, 2] = 1.0
+
+
+# define the colors
+cmap = mpl.colors.ListedColormap(['lightgray', 'mediumseagreen'])
+# create a normalize object the describes the limits of
+# each color
+bounds = [0., 0.5, 1.]
+norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+#plt.imshow(board_config.astype(int), interpolation='none', cmap=cmap, norm=norm)
+sns.heatmap(A.astype(int), cmap=cmap,cbar=False, linewidths=.5)
+
+plt.xlabel('Regions', fontsize=16)
+plt.ylabel("Regions", fontsize=16)
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+plt.savefig("figs/example-adj-mtx.pdf")
+
