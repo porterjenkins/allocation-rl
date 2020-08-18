ff28ab78bee37be7becfb30878f9ce424885401d
Porter <porterjenkins@gmail.com>
2020-08-18 14:40:31 -0600

    add replay buffer. Take mean of posterior

--- mopo/mopo.py
+++ mopo/mopo.py
@@ -16,35 +16,40 @@ class Mopo(object):
         self.lmbda = lmbda
         self.buffer = ReplayBuffer(buffer_size)
 
+        # TODO: init data buffer with transition from static data
+
 
     def get_penalized_reward(self, r, lmbda):
         variance = np.var(r)
-        return r - lmbda*variance
+        mean = r.mean()
+        return mean - lmbda*variance
 
     def learn(self):
 
-        results = {'rewards': [0.0]}
-
         for i in range(self.epochs):
-            state = State.init_state(config=cfg.vals)
+            state = env.reset()
 
             for h in range(self.rollout):
-                #feasible_actions = AllocationEnv.get_feasible_actions(state["board_config"])
-                #action_mask = AllocationEnv.get_action_mask(feasible_actions, self.n_actions)
+                feasible_actions = AllocationEnv.get_feasible_actions(state["board_config"])
+                action_mask = AllocationEnv.get_action_mask(feasible_actions, self.n_actions)
 
                 # sample action a_j ~ pi(s_j)
-                #action, _states = self.policy.predict(state, mask=action_mask)
+                action, _states = self.policy.predict(state, mask=action_mask)
                 action = env.action_space.sample()
                 #action = AllocationEnv.check_action(state['board_config'], action)
 
                 # compute dynamics from env model
-                new_state, r_hat, dones, info = self.env_model.step([action])
+                new_state, r_hat, dones, info = self.env_model.step(action)
 
                 reward = self.get_penalized_reward(r_hat, self.lmbda)
 
-                results['rewards'].append(reward + results['rewards'][-1])
 
-                # TODO: add (s, a, r, s') to buffer
+                # add (s, a, r, s') to buffer
+                self.buffer.add(obs_t=state,
+                                action=action,
+                                reward=reward,
+                                obs_tp1=new_state,
+                                done=dones)
 
                 state = new_state
 
@@ -54,13 +59,14 @@ class Mopo(object):
 if __name__ == "__main__":
     prior = Prior(config=cfg.vals)
     env = AllocationEnv(config=cfg.vals, prior=prior, load_model=True, full_posterior=True)
+    policy =
 
     mopo = Mopo(policy=None,
                 env_model=env,
                 epochs=10,
                 rollout=5,
                 n_actions = env.n_actions,
-                lmbda=.1
+                lmbda=1e-3
     )
 
     mopo.learn()
