706505e75253afffab4cc7e6d8c9169c847fb8a3
Porter <porterjenkins@gmail.com>
2020-08-19 10:57:45 -0600

    save memory buffer from random exploration

--- experiments/random_action.py
+++ experiments/random_action.py
@@ -1,30 +1,46 @@
+import json
 import numpy as np
+import matplotlib.pyplot as plt
+import pickle
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
 from envs.prior import Prior
 from envs.allocation_env import AllocationEnv
 import config.config as cfg
-import matplotlib.pyplot as plt
 from utils import serialize_floats
-import json
+from stable_baselines.deepq.replay_buffer import ReplayBuffer
+from envs.state import State
+
 
 TIME_STEPS = cfg.vals["episode_len"]
 prior = Prior(config=cfg.vals)
 env = AllocationEnv(config=cfg.vals, prior=prior, load_model=True)
 results = {'rewards': [0.0]}
+buffer = ReplayBuffer(size=50000)
 
 obs = env.reset()
 for i in range(TIME_STEPS):
     action = env.action_space.sample()
     action = AllocationEnv.check_action(obs['board_config'], action)
-    obs, rew, dones, info = env.step(action)
+    new_obs, rew, dones, info = env.step(action)
     print("Timestep: {}".format(i))
     print("action: {} - reward: {}".format(action, rew))
     print(obs['day_vec'])
     print(obs['board_config'])
 
     results['rewards'].append(rew + results['rewards'][-1])
+
+    # add (s, a, r, s') to buffer
+    buffer.add(obs_t=State.get_vec_observation(obs),
+               action=action,
+               reward=rew,
+               obs_tp1=State.get_vec_observation(new_obs),
+               done=float(dones))
+
+    obs = new_obs
 
 print(results['rewards'])
 
@@ -42,3 +58,6 @@ for k, v in results.items():
 
 with open("output/random-policy-{}.json".format(cfg.vals['prj_name']), 'w') as f:
     json.dump(results, f)
+
+with open("../data/random-buffer.p", 'wb') as f:
+    pickle.dump(buffer, f)
