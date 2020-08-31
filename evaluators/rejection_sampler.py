5f22f7f181cbeae427127be0240869902945d6c6
Porter <porterjenkins@gmail.com>
2020-08-31 16:50:58 -0600

    implement MOPO policy in rejection sampler

--- evaluators/rejection_sampler.py
+++ evaluators/rejection_sampler.py
@@ -3,10 +3,14 @@ import numpy as np
 import random
 import torch
 
+import config.config as cfg
+
 from evaluators.eval_queue import EvalQueue
-from mopo.mopo import  Mopo
-from policies.deepq.policies import MlpPolicy
+from evaluators.mlp_env_model import MLPClassifer
+
 from policies.deepq.dqn import DQN
+from envs.allocation_env import AllocationEnv
+from envs.state import State
 
 def get_buffer(fpath):
     with open(fpath, 'rb') as f:
@@ -40,12 +44,14 @@ class PSRS(object):
 
     """Per-state Rejection Sampling evaluator"""
 
-    def __init__(self, buffer_path, policy, env_policy, n_actions, n_episodes):
+    def __init__(self, buffer_path, policy, env_policy, n_actions, n_regions, n_products, n_episodes):
 
         self.buffer_path = buffer_path
         self.policy = policy
         self.env_policy = env_policy
         self.n_actions = n_actions
+        self.n_regions = n_regions
+        self.n_products = n_products
         self.n_episodes = n_episodes
 
         self.buffer = get_buffer(self.buffer_path)
@@ -73,9 +79,10 @@ class PSRS(object):
 
         return queue
 
-    def get_m(self, state):
+    def get_m(self, state, mask):
 
-        prob_policy = self.policy.predict_proba(state)
+
+        prob_policy = self.policy.proba_step(state.reshape(1, -1), mask=mask)[0]
         prob_env = self.env_policy.predict_proba(state)
 
         M = -1
@@ -99,8 +106,14 @@ class PSRS(object):
             state = state[0]
 
             while True:
+                board_cfg = State.get_board_config_from_vec(state,
+                                                            n_regions=self.n_regions,
+                                                            n_products=self.n_products
+                                                            )
+                feasible_actions = AllocationEnv.get_feasible_actions(board_cfg)
+                action_mask = AllocationEnv.get_action_mask(feasible_actions, self.n_actions)
 
-                M = self.get_m(state)
+                M = self.get_m(state, action_mask)
 
                 try:
                     _, a, r, s_prime = self.queue[state].pop()
@@ -109,7 +122,7 @@ class PSRS(object):
 
                 alpha = random.random()
 
-                prob_policy = self.policy.predict_proba(state)[a]
+                prob_policy = self.policy.proba_step(state.reshape(1, -1), mask=action_mask)[0][a]
                 prob_env = self.env_policy.predict_proba(state)[a]
 
                 rejection_tol = (1/M) * prob_policy/prob_env
@@ -135,12 +148,8 @@ if __name__ == "__main__":
     model_path = "../data/env_policy.pt"
 
     mopo_dqn = DQN.load(f"../experiments/models/mopo-policy.p")
-
-    policy = Policy(A, buffer_path, model_path)
-
-
     env_policy = Policy(A, buffer_path, model_path)
 
 
-    psrs = PSRS(buffer_path, policy, env_policy, A, 10)
+    psrs = PSRS(buffer_path, mopo_dqn, env_policy, A, cfg.vals["n_regions"], cfg.vals["n_products"], 10)
     psrs.evaluate()
